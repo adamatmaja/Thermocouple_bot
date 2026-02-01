3cedf9f3b8822ea374bc52d45477b527e4472399
adamatmaja <work.adamatmaja@gmail.com>
2026-02-01 09:48:35 +0700

    Update thermocouple.ino

--- thermocouple.ino
+++ thermocouple.ino
@@ -10,17 +10,16 @@
 #define MAX6675_SO D3
 
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-
-IPAddress ip(10,184,32,100);
+IPAddress ip(10,1,3,1);
 IPAddress dns(8,8,8,8);
-IPAddress gateway(10,184,32,254);
+IPAddress gateway(10,1,3,2);
 IPAddress subnet(255,255,255,0);
 
 // ===== MQTT =====
-IPAddress mqttServer(10, 184, 33, 238);   // IP Server
+IPAddress mqttServer(10, 1, 10, 1);   // IP Server
 #define MQTT_PORT 1883
-#define MQTT_TOPIC "TOP/temp"
-#define MQTT_CLIENT_ID "ESP8266_ESP01"
+#define MQTT_TOPIC "tes/tele/ESP8266_01"
+#define MQTT_CLIENT_ID "ESP8266_ESP01_XX"
 
 // ===== OBJECT =====
 MAX6675 thermocouple(MAX6675_SCK,MAX6675_CS,MAX6675_SO);
@@ -28,7 +27,7 @@ EthernetClient ethClient;
 PubSubClient mqttClient(ethClient);
 
 // ===== DEVICE ID =====
-String deviceId;
+#define DEVICE_ID "ESP8266_01"
 
 // ===== MQTT CONNECT =====
 void reconnectMQTT() {
@@ -47,13 +46,6 @@ void reconnectMQTT() {
 void setup() {
   Serial.begin(9600);
   delay(1000);
-
-  Serial.println("===DEVICE STARTING===");
-
-  // Ambil CHIP ID
-  deviceId = "ESP8266_01";
-  Serial.print("DEVICE ID = ");
-  Serial.println(deviceId);
 
   WiFi.mode(WIFI_OFF);
   WiFi.forceSleepBegin();
@@ -101,7 +93,7 @@ void loop(){
   char payload[128];
   snprintf(payload, sizeof(payload),
     "{\"device_id\":\"%s\",\"temperature\":%.2f}",
-    deviceId.c_str(),
+    DEVICE_ID,
     tempC
   );
 
